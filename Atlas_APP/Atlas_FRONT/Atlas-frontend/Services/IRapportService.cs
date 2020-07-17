171b1e697011affd37d3e7b961eab9b27a74aceb
AmineSadfi <amine.sadfi@keyrus.com>
2020-07-17 22:41:43 +0100

    interfaces

--- Atlas_APP/Atlas_FRONT/Atlas-frontend/Services/IRapportService.cs
+++ Atlas_APP/Atlas_FRONT/Atlas-frontend/Services/IRapportService.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Atlas_frontend.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Atlas_frontend.Services
 {
-    interface IRapportService
+    interface IRapportService: IBaseEntityService<RapportModel>
     {
     }
 }
