17dfe300467c2fd60bd200ab35d9a047c1ea38af
AmineSadfi <amine.sadfi@keyrus.com>
2020-07-19 12:49:51 +0100

    projet

---
+++ Atlas_APP/Atlas_FRONT/Atlas-frontend/Controllers/ProjetController.cs
@@ -0,0 +1,23 @@
+﻿using Atlas_frontend.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Atlas_frontend.Controllers
+{
+    public class ProjetController : Controller
+    {
+        private IProjetService _projetService;
+        public ProjetController(IProjetService projetService)
+        {
+            _projetService = projetService;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
